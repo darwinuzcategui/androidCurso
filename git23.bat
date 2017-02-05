381fa70eebac4574394c7e018e8bdc368aade590
unknown <darwin.uzcategui1973@gmail.com>
2017-02-05 11:27:11 -0430

    demo5

--- git23.bat
+++ git23.bat
@@ -2,5 +2,5 @@ echo "# androicurso >>reame.txt
 git init
 git add .
 git commit -m "demo5"
-git remote add androidCurso https://github.com/darwinuzcategui/androidCurso.git
-git push -u androidCurso master
+git remote add CursoAndroid https://github.com/darwinuzcategui/androidCurso.git
+git push -u CursoAndroid master
