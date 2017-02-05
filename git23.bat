afed30b7fa51f400d0baf1ca69a0ec4db32bc2e8
unknown <darwin.uzcategui1973@gmail.com>
2017-02-05 11:06:23 -0430

    demo4

--- git23.bat
+++ git23.bat
@@ -1,6 +1,6 @@
 echo "# androicurso >>reame.txt
 git init
 git add .
-git commit -m "demo3"
-git remote add origin https://github.com/darwinuzcategui/maxCurso.git
+git commit -m "demo4"
+git remote add origin https://github.com/darwinuzcategui/maxCurso1.git
 git push -u origin master
